2e3f18450be044f5ed855f69a1ca9db8083dc4eb
Keyur Patel <patelplayer@gmail.com>
2014-11-15 13:18:22 -0500

    Almost working socket.io

--- bin/simplimy.js
+++ bin/simplimy.js
@@ -1,33 +1,64 @@
-module.exports = function(ready_callback, connect_callback, frame_callback, disconnect_callback) 
+module.exports = function(io) 
 {
 	var simplimy = require('myojs/template/entry.js');
+
 	var hub = new Myo.Hub();
 	var locked = true;
 
 	hub.on('ready', function() 
 	{
-		ready_callback();
+		io.on('connection', function(socket)
+	    {
+	    	socket.emit('ready', true);
+	    });
 	});
 
 	hub.on('connect', function() 
 	{
-	    connect_callback();
+	    io.on('connection', function(socket)
+	    {
+	    	socket.emit('connected', true);
+	    	locked = false;
+	    });
 	});
 
 	hub.on('frame', function(frame) 
 	{
-	    if(!locked && frame.pose.valid)
-	    {
-		    if(frame.pose.type >= 0 && frame.pose.type < 5)
-		    {
-		    	frame_callback(frame.pose.type);
-		    }
+		var ret = -1;
+		if(!locked && frame.pose.valid)
+    	{
+	    	if(frame.pose.type >= 0 && frame.pose.type < 5)
+	    	{
+	    		ret = frame.pose.type;
+	    	}
 			locked = true;
 		}
+		if(ret > -1)
+		{
+			console.log(ret);
+		}
+		io.on('connection', function (socket) 
+		{
+			socket.emit('movement', ret);
+		});
 	});
 
 	hub.on('disconnect', function() 
 	{
-	    disconnect_callback();
+	    io.on('connection', function(socket)
+	    {
+	    	socket.disconnect();
+	    });
 	});
+
+	io.on('connection', function(socket)
+	{
+		socket.on('unlock', function(bool)
+		{
+			console.log(locked);
+			locked = bool;
+		});
+	});
+
+	
 }
