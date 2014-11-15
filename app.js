1d8798d77e25573e23e6319debccfb13084e9f54
Keyur Patel <patelplayer@gmail.com>
2014-11-15 09:11:54 -0500

    Added homepage with graphics. Work in progress - Getting Started.

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var landing = require('./routes/landing');
+var index = require('./routes/index');
+var getting_started = require('./routes/getting_started');
 var myo = require('./bin/simplimy.js');
 
 var app = express();
@@ -23,8 +23,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/landing', landing);
+app.use('/', index);
+app.use('/getting_started', getting_started);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
