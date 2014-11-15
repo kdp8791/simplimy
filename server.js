47c27c271bd2a47e0af5392152df79f22e709083
Keyur Patel <patelplayer@gmail.com>
2014-11-15 03:02:27 -0500

    Added myo basic pose reads.

--- server.js
+++ server.js
@@ -11,5 +11,8 @@ app.use(express.static(__dirname + '/public'));
 
 require('./app/routes.js')(app);
 
+var myo = require('./app/simplimy.js');
+
 app.listen(8080);
 console.log("App listening on port 8080");
+
