8ef1cbddbbc6ac480ea4589f4eac92e3bc28b24d
Keyur Patel <patelplayer@gmail.com>
2014-11-15 01:11:16 -0500

    Added server.js. Updated package.json to read server.js on boot.

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override'); 
+
+mongoose.connect('mongodb://keyur:wahjudi@ds053320.mongolab.com:53320/simplimy');
+app.listen(8080);
+console.log("App listening on port 8080");
